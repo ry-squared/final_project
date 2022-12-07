cf52cb4c2dd2a17e0f72312ea9d81671ef3b981d
Ryan West <raw.ryanawest@gmail.com>
2022-12-07 01:30:12 -0500

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,59 +13,30 @@ import os
 import zipfile
 import tempfile
 
+"""
+Upload Model Below
+"""
+
 stream = st.file_uploader('Choose a model', type='zip')
-if stream is not None:
-  myzipfile = zipfile.ZipFile(stream)
-  with tempfile.TemporaryDirectory() as tmp_dir:
-    myzipfile.extractall(tmp_dir)
-    root_folder = myzipfile.namelist()[0] # e.g. "model.h5py"
-    model_dir = os.path.join(tmp_dir, root_folder)
-    model = keras.models.load_model(model_dir)
+@st.experimental_singleton
+def load_model_from_zip():
+    if stream is not None:
+      myzipfile = zipfile.ZipFile(stream)
+      with tempfile.TemporaryDirectory() as tmp_dir:
+        myzipfile.extractall(tmp_dir)
+        root_folder = myzipfile.namelist()[0] # e.g. "model.h5py"
+        model_dir = os.path.join(tmp_dir, root_folder)
+        model = keras.models.load_model(model_dir)
+    return model
+model = load_model_from_zip()
 
-
-st.write((os.getcwd()))
-st.write((os.listdir()))
-
-# """
-# # Welcome to Streamlit!
-#
-# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-#
-# If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-# forums](https://discuss.streamlit.io).
-#
-# In the meantime, below is an example of what you can do with just a few lines of code:
-# """
+if model is not None:
+    st.subheader("Model already loading.. skip model upload")
 
 """
 Upload Patient Xray below
 """
 
-
-# model = load_model("models_deploy/Detect_Covid-2022-11-28--02-30-48.h5")
-# @st.cache
-
-# with st.echo(code_location='below'):
-#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-#
-#     Point = namedtuple('Point', 'x y')
-#     data = []
-#
-#     points_per_turn = total_points / num_turns
-#
-#     for curr_point_num in range(total_points):
-#         curr_turn, i = divmod(curr_point_num, points_per_turn)
-#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-#         radius = curr_point_num / total_points
-#         x = radius * math.cos(angle)
-#         y = radius * math.sin(angle)
-#         data.append(Point(x, y))
-#
-#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-#         .mark_circle(color='#0068c9', opacity=0.5)
-#         .encode(x='x:Q', y='y:Q'))
-# model_file = st.file_uploader("Choose a Model")
 uploaded_file = st.file_uploader("Choose a file")
 if (uploaded_file is not None) and (model is not None):
     # model = load_model(model_file)
@@ -85,12 +56,4 @@ if (uploaded_file is not None) and (model is not None):
     y_pred_proba = max(predict_x[0])
     st.title(f"Patient XYZ Diagnosis: {inv_classes[y_pred].upper()} xray")
     st.header(f"{str(round(y_pred_proba,4))} confidence")
-
     st.image(image, caption=f"Patient XYZ Diagnosis: {inv_classes[y_pred].upper()} xray \n {y_pred_proba} confidence")
-
-# import urllib.request
-# @st.experimental_singleton
-# def load_model():
-#     if not os.path.isfile('model.h5'):
-#         urllib.request.urlretrieve('https://github.gatech.edu/rwest61/xray-classification/blob/master/models_deploy/Detect_Covid-2022-11-28--02-30-48.h5', 'model2.h5')
-#     return keras.models.load_model('model.h5')
